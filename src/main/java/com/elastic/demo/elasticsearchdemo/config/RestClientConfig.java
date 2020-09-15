1c107496ff37db5bfefc1920c2c1975b42d4ffb8
patel-uday <pateluday07@gmail.com>
2020-09-15 22:57:37 +0530

    add dev profile and move config values in the .yml file

--- src/main/java/com/elastic/demo/elasticsearchdemo/config/RestClientConfig.java
+++ src/main/java/com/elastic/demo/elasticsearchdemo/config/RestClientConfig.java
@@ -1,6 +1,7 @@
 package com.elastic.demo.elasticsearchdemo.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -9,10 +10,13 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
 @Configuration
 public class RestClientConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${elasticsearch.connection.url}")
+    private String connectionUrl;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(connectionUrl)
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
