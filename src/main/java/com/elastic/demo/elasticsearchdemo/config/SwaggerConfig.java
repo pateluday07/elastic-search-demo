9ac47dab6f3053f8d0bc606369a83db182cfe410
patel-uday <pateluday07@gmail.com>
2020-09-29 23:32:00 +0530

    add swagger

---
+++ src/main/java/com/elastic/demo/elasticsearchdemo/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.elastic.demo.elasticsearchdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfo(
+                "Elastic Search Demo",
+                "Elastic Search Demo With Spring Boot",
+                "1.0",
+                "",
+                new Contact("Uday Patel", "", "pateluday07@gmail.com"),
+                "",
+                "",
+                Collections.emptyList());
+    }
+
+}
